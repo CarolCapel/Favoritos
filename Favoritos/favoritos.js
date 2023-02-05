455b18947db21e8e81dcdc8938d63a23e1b6d339
Carol Capel <108011375+CarolCapel@users.noreply.github.com>
2023-02-05 00:38:07 -0300

    Add files via upload

---
+++ Favoritos/favoritos.js
@@ -0,0 +1,6 @@
+function adicionarFilme() {
+    var filmeFavorito = document.getElementById('filme').value
+    var elementoListaFilmes = document.getElementById('listaFilmes')
+    elementoListaFilmes.innerHTML = elementoListaFilmes.innerHTML + '<img src=' + filmeFavorito + '>'
+    document.getElementById('filme').value = ''
+}
