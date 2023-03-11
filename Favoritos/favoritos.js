ec652469a7ecc7d57505420d5d5ae6dbb637aa67
Carol Capel <108011375+CarolCapel@users.noreply.github.com>
2023-03-11 11:31:33 -0300

    Delete Favoritos directory

--- Favoritos/favoritos.js
+++
@@ -1,6 +0,0 @@
-function adicionarFilme() {
-    var filmeFavorito = document.getElementById('filme').value
-    var elementoListaFilmes = document.getElementById('listaFilmes')
-    elementoListaFilmes.innerHTML = elementoListaFilmes.innerHTML + '<img src=' + filmeFavorito + '>'
-    document.getElementById('filme').value = ''
-}
